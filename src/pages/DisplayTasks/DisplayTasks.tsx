a810ec5b6625a9d0dcaa40990405943868c376d7
Eslashier <anfelov@gmail.com>
2022-06-27 20:50:33 -0500

    table with moked info works

--- src/pages/DisplayTasks/DisplayTasks.tsx
+++ src/pages/DisplayTasks/DisplayTasks.tsx
@@ -17,11 +17,17 @@ const DisplayTasks = () => {
     return(
         <div>
         <h1>Hello from Display Tasks</h1>
-        <h2 >Id: {localState.taskDetail.taskId}</h2>
-        <h2 >Last Name: {localState.taskDetail.last_name}</h2>
-        <h2 >E-mail: {localState.taskDetail.email}</h2>
-        <h2 >Gender: {localState.taskDetail.gender}</h2>
-        <h2 >Ip Address: {localState.taskDetail.ip_address}</h2>
+        <h2 >Id de Tarea: {localState.taskDetail.taskId}</h2>
+        <h2 >Nombre de tarea: {localState.taskDetail.name}</h2>
+        <h2 >Id Proyecto: {localState.taskDetail.projectId}</h2>
+        <h2 >Nombre de Proyecto: {localState.taskDetail.projectName}</h2>
+        <h2 >Fecha de inicio: {localState.taskDetail.date}</h2>
+        <h2 >Fecha de finalización: {localState.taskDetail.endDate}</h2>
+        <h2 >Tags: {localState.taskDetail.labels}</h2>
+        <h2 >Descripción: {localState.taskDetail.description}</h2>
+        <h2 >Archivos adjuntos: {localState.taskDetail.urls}</h2>
+        <h2 >Estado de tarea: {localState.taskDetail.state}</h2>
+        <h2 >Desarrolladores asociados: {localState.taskDetail.developerEmails}</h2>
         <Link to='/'>
                     Volver
         </Link>
