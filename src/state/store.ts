c03584bff487be93fa1083fc9bf892c684c89f3d
Angel Agelvis <agelvisangel@gmail.com>
2022-06-29 13:06:42 -0500

    Merge with development completed

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
 import { configureStore, compose, combineReducers } from "@reduxjs/toolkit";
 import thunk from 'redux-thunk';
-=======
-import { configureStore } from "@reduxjs/toolkit";
-import projectReducer from "./slice/projectSlice";
->>>>>>> b7983241d3120be81d646e027e1afed2c8628024
 import { useDispatch } from 'react-redux'
 import loginReducer from "./slice/loginSlice";
 import taskReducer from "./slice/taskSlice"
 import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
 import sessionStorage from "redux-persist/es/storage/session";
+import projectReducer from "./slice/projectSlice";
 
 
 const persistConfig = {
@@ -20,24 +16,17 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   tasks: taskReducer,
-  login: loginReducer
+  login: loginReducer,
+  projects: projectReducer,
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-<<<<<<< HEAD
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(thunk)
-=======
-    reducer: {
-        tasks: taskReducer,
-        projects: projectReducer,
-        login: loginReducer
-    }
->>>>>>> b7983241d3120be81d646e027e1afed2c8628024
 })
 
 export const persistor = persistStore(store)
