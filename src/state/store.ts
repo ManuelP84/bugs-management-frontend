1be0e1aebc9dc40e893de6790903ad321c2c6467
Angel Agelvis <agelvisangel@gmail.com>
2022-06-29 16:59:22 -0500

    FIX
    Login with google, store's typescript warnings

--- src/state/store.ts
+++ src/state/store.ts
@@ -26,7 +26,9 @@ export const store = configureStore({
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(thunk)
+    getDefaultMiddleware({
+      serializableCheck: false
+    }).concat(thunk)
 })
 
 export const persistor = persistStore(store)
