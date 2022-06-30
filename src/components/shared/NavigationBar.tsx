975512e6b5a038daa646824d761735718a3d12ea
Angel Agelvis <agelvisangel@gmail.com>
2022-06-30 17:49:19 -0500

    ADD
    Bug delete function

--- src/components/shared/NavigationBar.tsx
+++ src/components/shared/NavigationBar.tsx
@@ -1,35 +1,49 @@
 import React, { useState } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap"
-import { Link, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectActualUser } from "../../state/slice/loginSlice";
 
-type Props = {}
+type Props = {};
 
 const NavigationBar: React.FC<Props> = (props) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const user = useSelector(selectActualUser());
 
-    const [showLoginModal, setShowLoginModal] = useState(false);
-
-    return (
-        <Navbar bg="secondary sticky-top" variant="light" expand="lg">
-            <Container>
-                <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
-                <Nav className="me-auto">
-                        <>
-                            <Nav.Link as={Link} to="/admin">Admin</Nav.Link>
-                            <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-                            <Nav.Link as={Link} to="/project">Projects</Nav.Link>
-                            <Nav.Link as={Link} to="/addbug">Task</Nav.Link>
-                            <Nav.Link as={Link} to="/task-list">Bugs</Nav.Link>
-                        </>
-                </Nav>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="secondary sticky-top" variant="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Home
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <>
+            {user?.userRol == "Admin" && (
+              <Nav.Link as={Link} to="/admin">
+                Admin
+              </Nav.Link>
+            )}
+            <Nav.Link as={Link} to="/dashboard">
+              Dashboard
+            </Nav.Link>
+            <Nav.Link as={Link} to="/project">
+              Projects
+            </Nav.Link>
+            <Nav.Link as={Link} to="/task-list">
+              Tasks
+            </Nav.Link>
+            <Nav.Link as={Link} to="/bugs">
+              Bugs
+            </Nav.Link>
+          </>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
-
