f66491c4ec5dc8bbffd3abb792ae025c777f3892
Eslashier <anfelov@gmail.com>
2022-06-30 14:15:45 -0500

    merged router

--- src/components/Routes/Router.tsx
+++ src/components/Routes/Router.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "../../pages/Login";
 import MainPage from "../../pages/MainPage";
-import SingUpPage from "../../pages/SignUp";
 import { RootState } from "../../state/store";
 import PublicRoutes from "./PublicRoutes";
 import "../../styles/login.css";
 import ProjectPage from "../../pages/ProjectPage";
-import DashboardPage from "../../pages/DashboardPage";
-import { useSelector } from "react-redux";
-import ListOfTasks from "../../pages/ListOfTasks/ListOfTask";
-import DisplayTasks from "../../pages/DisplayTasks/DisplayTask";
+import AdminPage from "../../pages/AdminPage";
+import BugsPage from "../../pages/BugsPage";
+import ListOfTasks from "../../pages/ListOfTasks/ListOfTasks";
 import CreateTask from "../../pages/CreateTask/CreateTask";
 import UpdateTask from "../../pages/UpdateTask/UpdateTask";
+import DisplayTasks from "../../pages/DisplayTasks/DisplayTasks";
+import DashboardPage from "../../pages/DashboardPage";
+import { selectActualUser } from "../../state/slice/loginSlice";
+import AdminRoutes from "./AdminRoutes";
+import UsersRoutes from "./UsersRoutes";
 
 
 
@@ -22,27 +24,18 @@ const Router: React.FunctionComponent<IRoutesProps> = () => {
     const logged: boolean = useSelector(
         (state: RootState) => state.login.isLogged
     );
+    const user = useSelector(selectActualUser())
 
   return (
     <div className="App">
-      {logged ? (
-        <Routes>
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/project" element={<ProjectPage />} />
-          <Route path="*" element={<MainPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path='/task-list' element={<ListOfTasks />} />
-          <Route path='/task-detail' element={<DisplayTasks />} />
-          <Route path='/create-task' element={<CreateTask />} />
-          <Route path='/edit-task' element={<UpdateTask />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/signUp" element={<SingUpPage />} />
-          <Route path="*" element={<LoginPage />} />
-
-        </Routes>
+      {logged && user?.userRol == "Admin" ? (
+        <AdminRoutes />
+      )
+       : logged && user?.userRol != "Admin" ?(
+        <UsersRoutes />
+       )
+       : (
+        <PublicRoutes />
       )}
     </div>
   );
