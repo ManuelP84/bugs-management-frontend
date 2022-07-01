d5cc8316506cd2765e12a86ea3e9f87ecf7d66a0
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-30 19:42:02 -0500

    user picture included in the navbar

--- src/components/shared/NavigationBar.tsx
+++ src/components/shared/NavigationBar.tsx
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar } from "react-bootstrap"
 import { Link, useNavigate } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../../state/store";
+import profile from "../../assets/profile.png";
 
 type Props = {}
 
@@ -11,6 +12,7 @@ const NavigationBar: React.FC<Props> = (props) => {
     const navigate = useNavigate();
 
     const user = useSelector((state: RootState) => state.login.actualUser);
+    console.log(user.userImage)
 
     return (
         <Navbar bg="secondary sticky-top" variant="light" expand="lg">
@@ -26,6 +28,22 @@ const NavigationBar: React.FC<Props> = (props) => {
                     </>
                 </Nav>
                 <span>{user?.userEmail} ({user?.userRol ? user?.userRol : "Reader"})</span>
+                <Nav.Link as={Link} to="/project">
+                    {user.userImage ? (
+                    <img
+                        src={user.userImage}
+                        height="35"
+                        className="d-inline-block align-top rounded-circle"
+                    />
+                    ) : (
+                    <img
+                        src={profile}
+                        height="35"
+                        className="d-inline-block align-top rounded-circle"
+                    />
+                    )}
+          </Nav.Link>
+
             </Container>
         </Navbar>
     );
